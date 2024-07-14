8b9004d1aa6312c03a898b11ef1da038e702bd3c
Liam <liam@lokaisingh.com>
2024-07-14 15:24:52 -0400

    Added contact page and responsiveness

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
